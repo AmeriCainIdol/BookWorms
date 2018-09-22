510fe78bd87167b492b7ee0dbfb8eba32b855b93
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-09-22 15:22:26 -0500

    finished rebasing

--- client/src/components/UserReviewSubmit.jsx
+++ client/src/components/UserReviewSubmit.jsx
@@ -45,77 +45,75 @@ export default class UserReviewSubmit extends Component {
     e.preventDefault();
     this.setState({
       title: e.target.value
-    }, () => {
-      // console.log(this.state.title)
-    })
+    }, () => {})
   }  
   
   updateReviewText(e) {
     e.preventDefault();
     this.setState({
       reviewText: e.target.value
-    }, () => {
-      // console.log(this.state.reviewText)
-    })
+    }, () => {})
   }
   
   updateRating(e) {
     e.preventDefault();
     this.setState({
       rating: e.target.id
-    }, () => {
-      // console.log(this.state.rating)
-    })
-    .catch(err => {
-      console.error(err);
-    })
-    console.log(this.state.userReview)
-  }
+    }, () => {})
+  };
 
   render () {
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-4">
-              <form role="form" onSubmit={this.handleReviewSubmit}>
-                <div className="form-group">
-                  <div className="form-check form-check-inline">
-                    <input className="form-check-input" type="checkbox" id="1" onChange={this.updateRating}/>
-                    <label className="form-check-label" htmlFor="inlineCheckbox1">1</label>
-                    <input className="form-check-input" type="checkbox" id="2" onChange={this.updateRating}/>
-                    <label className="form-check-label" htmlFor="inlineCheckbox1">2</label>
-                    <input className="form-check-input" type="checkbox" id="3" onChange={this.updateRating}/>
-                    <label className="form-check-label" htmlFor="inlineCheckbox1">3</label>
-                    <input className="form-check-input" type="checkbox" id="4" onChange={this.updateRating}/>
-                    <label className="form-check-label" htmlFor="inlineCheckbox1">4</label>
-                    <input className="form-check-input" type="checkbox" id="5" onChange={this.updateRating}/>
-                    <label className="form-check-label" htmlFor="inlineCheckbox1">5</label>
-                  </div>
-                  <input  type="text" 
-                          className="form-control" 
-                          placeholder="Title"
-                          style={{width: '400px'}}
-                          onChange={this.updateTitle}/>
-                  <textarea type="text" 
-                            className="form-control" 
-                            onChange={this.updateReviewText}
-                            placeholder="Write Review...."
-                            style={{width: '400px'}}/>
-                  <button type="submit" 
-                          className="btn btn-success"
-                          >Submit Review</button>
+            <form role="form" onSubmit={this.handleReviewSubmit}>
+              <div className="form-group">
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="checkbox" id="1" onChange={this.updateRating}/>
+                  <label className="form-check-label" htmlFor="inlineCheckbox1">1</label>
+                  <input className="form-check-input" type="checkbox" id="2" onChange={this.updateRating}/>
+                  <label className="form-check-label" htmlFor="inlineCheckbox1">2</label>
+                  <input className="form-check-input" type="checkbox" id="3" onChange={this.updateRating}/>
+                  <label className="form-check-label" htmlFor="inlineCheckbox1">3</label>
+                  <input className="form-check-input" type="checkbox" id="4" onChange={this.updateRating}/>
+                  <label className="form-check-label" htmlFor="inlineCheckbox1">4</label>
+                  <input className="form-check-input" type="checkbox" id="5" onChange={this.updateRating}/>
+                  <label className="form-check-label" htmlFor="inlineCheckbox1">5</label>
                 </div>
-              </form>
-            </div>
+                <div>
+                <input  type="text" 
+                        className="form-control" 
+                        placeholder="Title"
+                        style={{width: '400px'}}
+                        onChange={this.updateTitle}/>
+                <textarea type="text" 
+                          className="form-control" 
+                          onChange={this.updateReviewText}
+                          placeholder="Write Review...."
+                          style={{width: '400px'}}/>
+                <button type="submit" 
+                        className="btn btn-success"
+                        >Submit Review</button>
+                </div>
+              </div>
+            </form>
           </div>
+        </div>
           <div className="container-fluid">
             <div className="row">
               <div className="col-md-4">
-              <UserDisplay review={this.state.userReview}/>
+                {this.state.userReview.map(reviewPart => {
+                  return (<div><h1>{reviewPart.title}</h1>
+                          <h2>{reviewPart.bookTitle}</h2>
+                          <h3>{reviewPart.reviewText}</h3>
+                          <h4>{reviewPart.rating}</h4></div>
+                      )
+                })}
               </div>
             </div>
           </div>
         </div>
     )
   }
-};
+}
